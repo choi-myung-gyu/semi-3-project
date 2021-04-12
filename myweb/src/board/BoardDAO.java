13b4525b135096aff4290d522c3182ee1459fbba
choi <choi@host.docker.internal>
2021-04-12 21:07:11 +0900

    D-3 로그인 기능은 id 전달을 위해 구성됨, 좋아요 기능 구현해야함, DBConnection 끊기는 오류 고쳐야함

--- myweb/src/board/BoardDAO.java
+++ myweb/src/board/BoardDAO.java
@@ -2,138 +2,480 @@ package board;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 import main.DBConnection;
 
 public class BoardDAO {
-	private final String table = "board_t";
 	private Connection connection = null;
-	private PreparedStatement pstat = null;
-	private ResultSet result;
-	
-	public BoardDAO() {
-//		DBConnection db_conn = new DBConnection("50000", "web_admin", "admin");
-//		this.connection = db_conn.getConnect();
-		this.connection = new DBConnection("50000", "adminsystem", "oracle").getConnect();
-	}
-	
-	public BoardVO getRecord(int id) {
-		BoardVO record = new BoardVO();
-		
-		String sql = "";
-		sql += "SELECT * FROM " + this.table;
-		sql += " WHERE id=?";
-		
-		try {
-			this.setPreStatement(sql);
-			this.pstat.setInt(1, id);
-			result = this.pstat.executeQuery();
-			if(result.next()) {
-				record.setRecord(result);
-			} else {
-				System.out.println("조회 결과가 없습니다.");
-			}
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-		
-		return record;
-	}
-	
-//	//bbsID 게시글 번호 가져오는 함수
-//		public int getNext() {
-//			//String SQL = "SELECT bbsID FROM dbo.[BBS] ORDER BY bbsID DESC";
-//			String SQL = "SELECT B_ID FROM board_t ORDER BY B_ID DESC";
-//			
-//			try {
-//				PreparedStatement pstmt = connection.prepareStatement(SQL);
-//				result = pstmt.executeQuery();
-//				if (result.next()) {
-//					return result.getInt(1) + 1;
-//				}
-//				return 1; // 첫번째 게시물인경우
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//			return -1;// 데이터베이스 오류
-//		}
-	
-	public ArrayList<BoardVO> getAll() {
-		ArrayList<BoardVO> records = new ArrayList<BoardVO>();
-		
-		String sql = "";
-		sql += "SELECT * FROM " + this.table;
-		sql += " ORDER BY id DESC";
-		
-		try {
-			this.setPreStatement(sql);
-			ResultSet result = this.pstat.executeQuery();
-			while(result.next()) {
-				BoardVO record = new BoardVO();
-				record.setRecord(result);
-				records.add(record);
-			}
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-		
-		return records;
-	}
-	
-	public boolean saveData(BoardVO data) {
-		boolean result = false;
-		
-		String sql="";
-		sql += "INSERT INTO board_t(id, btype, author, title, context)";
-		sql += " VALUES(board_seq.NEXTVAL,?,?,?,?)";
-		
-		try {
-			this.pstat=this.connection.prepareStatement(sql);
-			this.pstat.setString(1, data.getBtype());
-			this.pstat.setString(2, data.getAuthor());
-			this.pstat.setString(3, data.getTitle());
-			this.pstat.setString(4, data.getContext());
+
+	private static BoardDAO instance = new BoardDAO();
 			
-			if(this.pstat.executeUpdate() == 1) {
-				result = true;
+	public static BoardDAO getInstance() {
+		return instance;
+	}
+	
+	public BoardDAO(){
+		this.connection = new DBConnection("50000", "adminsystem", "oracle").getConnect();
+		
+	}
+
+	public void insertArticle(BoardVO article) throws Exception {
+		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		int num = article.getNum();
+		int ref = article.getRef();
+		int re_step = article.getRe_step();
+		int re_level = article.getRe_level();
+		int number=0;
+		String sql = "";
+		
+		try {
+			if(connection==null)
+				System.out.println("fail");
+			else
+				System.out.println("Connected");
+			sql = "select max(num) from boarde";
+			pstmt = connection.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				number = rs.getInt(1) + 1;
+			}else {
+				number = 1;
 			}
-			this.pstat.close();
-		} catch(SQLException e) {
-			e.printStackTrace();
+			closeDBResources(rs, pstmt);			
+			if(num!=0) {  
+				sql = "update boarde set re_step=re_step+1 where ref=? and re_step>?";
+				pstmt = connection.prepareStatement(sql);
+				pstmt.setInt(1, ref);
+				pstmt.setInt(2, re_step);
+				pstmt.executeUpdate();
+				re_step = re_step +1;
+				re_level = re_level+1;
+			}else {		
+				ref = number;
+				re_step = 0;
+				re_level = 0;
+			}
+			closeDBResources(rs, pstmt);			
+			sql="insert into boarde (num, writer, email, subject, passwd, reg_date, ";
+			sql+=" ref, re_step, re_level, content, ip, filename) values (?,?,?,?,?,?,?,?,?,?,?,?)";
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, number);
+            pstmt.setString(2, article.getWriter());
+            pstmt.setString(3, article.getEmail());
+            pstmt.setString(4, article.getSubject());
+            pstmt.setString(5, article.getPasswd());
+			pstmt.setTimestamp(6, article.getReg_date());
+            pstmt.setInt(7, ref);
+            pstmt.setInt(8, re_step);
+            pstmt.setInt(9, re_level);
+			pstmt.setString(10, article.getContent());
+			pstmt.setString(11, article.getIp());
+			pstmt.setString(12, article.getFilename());
+			pstmt.executeUpdate();
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			closeDBResources(rs, pstmt, connection);
+		}
+	}
+	
+	
+	public int getArticleCount() throws Exception{
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		int x = 0;
+		try {
+			
+			pstmt = connection.prepareStatement("select count(*) from boarde");
+			rs= pstmt.executeQuery();
+			
+			if(rs.next()) {
+				x=rs.getInt(1);
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			if(rs != null)try {rs.close();}catch(SQLException ex) {}
+			if(pstmt != null)try {pstmt.close();}catch(SQLException ex) {}
+			if(connection != null)try {connection.close();}catch(SQLException ex) {}
+		}
+		return x;
+	}
+	
+	
+	public List<BoardVO> getArticles(int start, int end) throws Exception{
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		List<BoardVO> articleList = null;
+		try {
+			
+			String sql = "SELECT * "
+					+ "FROM (SELECT ROWNUM rnum, B.*"
+					+ " FROM "
+					+ " (SELECT * FROM BOARDE ORDER BY ref desc, re_step asc ) B ) ";
+			sql += "WHERE rnum >= ? and rnum <= ?";
+			
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, start-1);
+			pstmt.setInt(2, end);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				articleList = new ArrayList<BoardVO>(end);
+				do {
+					BoardVO article = new BoardVO();
+					article.setNum(rs.getInt("num"));
+					article.setWriter(rs.getString("writer"));
+					article.setEmail(rs.getString("email"));
+					article.setSubject(rs.getString("subject"));
+					article.setPasswd(rs.getString("passwd"));
+					article.setReg_date(rs.getTimestamp("reg_date"));
+					article.setReadcount(rs.getInt("readcnt"));
+					article.setRef(rs.getInt("ref"));
+					article.setRe_step(rs.getInt("re_step"));
+					article.setRe_level(rs.getInt("re_level"));
+					article.setContent(rs.getString("content"));
+					article.setIp(rs.getString("ip"));
+					article.setFilename(rs.getString("filename"));
+					
+					articleList.add(article);
+				}while(rs.next());
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			if(rs != null)try {rs.close();}catch(SQLException ex) {}
+			if(pstmt != null)try {pstmt.close();}catch(SQLException ex) {}
+			if(connection != null)try {connection.close();}catch(SQLException ex) {}
+		}
+		
+		return articleList;
+	}
+	
+	public BoardVO getArticle(int num) throws Exception{
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		BoardVO article = null;
+		
+		try {
+			pstmt = connection.prepareStatement(
+					"update boarde set readcnt=readcnt+1 where num =?");
+				pstmt.setInt(1, num);
+				pstmt.executeUpdate();
+			
+			pstmt = connection.prepareStatement(
+					"select * from boarde where num = ?");
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				article= new BoardVO();				
+				article.setNum(rs.getInt("num"));
+				article.setWriter(rs.getString("writer"));
+				article.setEmail(rs.getString("email"));
+				article.setSubject(rs.getString("subject"));
+				article.setPasswd(rs.getString("passwd"));
+				article.setReg_date(rs.getTimestamp("reg_date"));
+				article.setReadcount(rs.getInt("readcnt"));
+				article.setRef(rs.getInt("ref"));
+				article.setRe_step(rs.getInt("re_step"));
+				article.setRe_level(rs.getInt("re_level"));
+				article.setContent(rs.getString("content"));
+				article.setIp(rs.getString("ip"));
+				article.setFilename(rs.getString("filename"));
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			if(rs != null)try {rs.close();}catch(SQLException ex) {}
+			if(pstmt != null)try {pstmt.close();}catch(SQLException ex) {}
+			if(connection != null)try {connection.close();}catch(SQLException ex) {}
+		}
+		return article;
+	}
+	
+	public BoardVO updateGetArticle(int num) throws Exception {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		BoardVO article = null;
+		
+		try {
+			pstmt = connection.prepareStatement(
+					"select * from boarde where num = ?");
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				article = new BoardVO();
+				article.setNum(rs.getInt("num"));
+				article.setWriter(rs.getString("writer"));
+				article.setEmail(rs.getString("email"));
+				article.setSubject(rs.getString("subject"));
+				article.setPasswd(rs.getString("passwd"));
+				article.setReg_date(rs.getTimestamp("reg_date"));
+				article.setReadcount(rs.getInt("readcnt"));
+				article.setRef(rs.getInt("ref"));
+				article.setRe_step(rs.getInt("re_step"));
+				article.setRe_level(rs.getInt("re_level"));
+				article.setContent(rs.getString("content"));
+				article.setIp(rs.getString("ip"));
+				article.setFilename(rs.getString("filename"));
+			}
+			
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			if(rs != null)try {rs.close();}catch(SQLException ex) {}
+			if(pstmt != null)try {pstmt.close();}catch(SQLException ex) {}
+			if(connection != null)try {connection.close();}catch(SQLException ex) {}
+		}
+		
+		return article;
+	}
+	
+	public int updateArticle(BoardVO article) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String dbpasswd = "";
+		String sql = "";
+		int x=-1;
+		try {
+			pstmt = connection.prepareStatement(
+					"select passwd from boarde where num = ?");
+			pstmt.setInt(1, article.getNum());
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dbpasswd = rs.getString("passwd");
+				if(dbpasswd.equals(article.getPasswd())) {
+					sql = "update boarde set writer=?, email=?, subject=?, passwd=?";
+					sql+=", content=? where num=?";
+					pstmt = connection.prepareStatement(sql);
+					
+					pstmt.setString(1, article.getWriter());
+					pstmt.setString(2, article.getEmail());
+					pstmt.setString(3, article.getSubject());
+					pstmt.setString(4, article.getPasswd());
+					pstmt.setString(5, article.getContent());
+					pstmt.setInt(6, article.getNum());
+					pstmt.executeQuery();
+					x =1;
+				}else {
+					x= 0;
+				}
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			if(rs != null)try {rs.close();}catch(SQLException ex) {}
+			if(pstmt != null)try {pstmt.close();}catch(SQLException ex) {}
+			if(conn != null)try {conn.close();}catch(SQLException ex) {}
+		}
+		return x;
+	}
+	public int deleteArticle(int num, String passwd) throws Exception{
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String dbpasswd = "";
+		int x = -1;
+		try {
+			pstmt = connection.prepareStatement(
+					"select passwd from boarde where num = ?");
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dbpasswd = rs.getString("passwd");
+				if(dbpasswd.equals(passwd)) {
+					pstmt = connection.prepareStatement("delete from boarde where num = ?");
+					pstmt.setInt(1, num);
+					pstmt.executeUpdate();
+					x = 1;	
+				}else {
+					x = 0;	
+				}
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			if(rs != null)try {rs.close();}catch(SQLException ex) {}
+			if(pstmt != null)try {pstmt.close();}catch(SQLException ex) {}
+			if(connection != null)try {connection.close();}catch(SQLException ex) {}
+		}
+		return x;
+	}
+	
+	public String checkIdPw(String id) throws Exception{
+		String dbpasswd = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		
+		try {
+			pstmt = connection.prepareStatement(
+					"select passwd from login where id = ?");
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dbpasswd = rs.getString("passwd");
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			closeDBResources(rs, pstmt, connection);
+		}
+		return dbpasswd;
+	}
+	
+	public int insertLogin(LoginVO article) throws Exception {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String id = article.getId();
+		String passwd = article.getPasswd();
+		String email = article.getEmail();
+		String tel = article.getTel();
+		String name = article.getName();
+		int result = 0;
+		String sql = "";
+		
+		try {
+			if(connection==null)
+				System.out.println("fail");
+			else
+				System.out.println("Connected");			
+			
+			sql="insert into login (id, passwd, email, tel, name, reg_date)";
+			sql+=" values (?,?,?,?,?,?)";
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, article.getId());
+            pstmt.setString(2, article.getPasswd());
+            pstmt.setString(3, article.getEmail());
+            pstmt.setString(4, article.getTel());
+            pstmt.setString(5, article.getName());
+			pstmt.setTimestamp(6, article.getReg_date());
+			result = pstmt.executeUpdate();
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			closeDBResources(rs, pstmt, connection);
 		}
 		return result;
 	}
 	
-	// 조회수 증가
-//	public void updateBoardCount(int boardNo) throws SQLException {
-//	    Connection con = null;
-//	    PreparedStatement pstmt = null;
-//	    try {
-//	        con = getConnection();
-//	        String sql = "update board set boardCount = boardCount+1 where boardNo=" + boardNo;
-//	        System.out.println(sql);
-//	        pstmt = con.prepareStatement(sql);
-//	        pstmt.executeUpdate();
-//	         
-//	    } catch (SQLException e) {
-//	        // TODO Auto-generated catch block
-//	        e.printStackTrace();
-//	    } finally {
-//	        closeAll(pstmt, con);
-//	    }
-//	}
-	public void close() {
-		// 모든 JDBC 관련 생성 객체 정보 close()
-		try {
-			this.pstat.close();
-			this.connection.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
+	private void closeDBResources(ResultSet rs, Statement stmt, Connection conn) {
+		if (rs != null) {
+			try {
+				rs.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	
+		if(stmt != null) {
+			try {
+				stmt.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if(conn != null) {
+			try {
+				conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
-
-
-	private void setPreStatement(String sql) throws SQLException {
-		this.pstat = this.connection.prepareStatement(sql);
+	private void closeDBResources(ResultSet rs, PreparedStatement pstmt, Connection conn) {
+		if (rs != null) {
+			try {
+				rs.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if(pstmt != null) {
+			try {
+				pstmt.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if(conn != null) {
+			try {
+				conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
+	private void closeDBResources(PreparedStatement pstmt, Connection conn) {
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if(conn != null) {
+			try {
+				conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	private void closeDBResources(Statement stmt, Connection conn) {
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	private void closeDBResources(ResultSet rs, PreparedStatement pstmt) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
+
--- myweb/src/board/BoardVO.java
+++ myweb/src/board/BoardVO.java
@@ -1,113 +1,102 @@
 package board;
 
-import java.sql.Date;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.Timestamp;
 
 public class BoardVO {
-	private int id;
-	private String btype;
-	private String author;
-	private String title;
-	private String context;
-	private Date create_date;
-	private Date update_date;
-	private int view_cnt;
-	private int like_cnt;
-	private int dislike_cnt;
-	
-	public int getId() {
-		return id;
+	private int num;
+	private String writer;
+	private String subject;
+	private String email;
+	private String content;
+	private String passwd;
+	private Timestamp reg_date;
+	private int readcount;
+	private String ip;
+	private int ref;
+	private int re_step;
+	private int re_level;
+	private String filename;
+
+	/*
+	 * private String name; private String id; private String tel;
+	 */
+	public int getNum() {
+		return num;
 	}
-	
-	public void setId(int id) {
-		this.id = id;
+	public void setNum(int num) {
+		this.num = num;
 	}
-	
-	public String getBtype() {
-		return btype;
+	public String getWriter() {
+		return writer;
 	}
-	
-	public void setBtype(String btype) {
-		this.btype = btype;
+	public void setWriter(String writer) {
+		this.writer = writer;
 	}
-	
-	public String getAuthor() {
-		return author;
+	public String getSubject() {
+		return subject;
 	}
-	
-	public void setAuthor(String author) {
-		this.author = author;
+	public void setSubject(String subject) {
+		this.subject = subject;
 	}
-	
-	public String getTitle() {
-		return title;
+	public String getEmail() {
+		return email;
 	}
-	
-	public void setTitle(String title) {
-		this.title = title;
+	public void setEmail(String email) {
+		this.email = email;
 	}
-	
-	public String getContext() {
-		return context;
+	public String getContent() {
+		return content;
 	}
-	
-	public void setContext(String context) {
-		this.context = context;
+	public void setContent(String content) {
+		this.content = content;
 	}
-	
-	
-	public Date getCreateDate() {
-		return create_date;
+	public String getPasswd() {
+		return passwd;
 	}
-	
-	public void setCreateDate(Date create_date) {
-		this.create_date = create_date;
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
 	}
-	
-	public Date getUpdateDate() {
-		return update_date;
+	public Timestamp getReg_date() {
+		return reg_date;
 	}
-	
-	public void setUpdateDate(Date update_date) {
-		this.update_date = update_date;
+	public void setReg_date(Timestamp reg_date) {
+		this.reg_date = reg_date;
 	}
-	
-	public int getViewCnt() {
-		return view_cnt;
+	public int getReadcount() {
+		return readcount;
 	}
-	
-	public void setViewCnt(int view_cnt) {
-		this.view_cnt = view_cnt;
+	public void setReadcount(int readcount) {
+		this.readcount = readcount;
 	}
-	
-	public int getLikeCnt() {
-		return like_cnt;
+	public String getIp() {
+		return ip;
 	}
-	
-	public void setLikeCnt(int like_cnt) {
-		this.like_cnt = like_cnt;
+	public void setIp(String ip) {
+		this.ip = ip;
 	}
-	
-	public int getDislikeCnt() {
-		return dislike_cnt;
+	public int getRef() {
+		return ref;
 	}
-	
-	public void setDislikeCnt(int dislike_cnt) {
-		this.dislike_cnt = dislike_cnt;
+	public void setRef(int ref) {
+		this.ref = ref;
 	}
-	
-	public void setRecord(ResultSet record) throws SQLException {
-		this.id = record.getInt("id");
-		this.btype = record.getString("btype");
-		this.author = record.getString("author");
-		this.title = record.getString("title");
-		this.context = record.getString("context");
-		this.create_date = record.getDate("create_date");
-		this.update_date = record.getDate("update_date");
-		this.view_cnt = record.getInt("view_cnt");
-		this.like_cnt = record.getInt("like_cnt");
-		this.dislike_cnt = record.getInt("dislike_cnt");
+	public int getRe_step() {
+		return re_step;
 	}
-	
+	public void setRe_step(int re_step) {
+		this.re_step = re_step;
+	}
+	public int getRe_level() {
+		return re_level;
+	}
+	public void setRe_level(int re_level) {
+		this.re_level = re_level;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
 }
